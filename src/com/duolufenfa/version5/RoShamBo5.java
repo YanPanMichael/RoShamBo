ab0834dbab798c945c2616d85b3fa2b4d9bc69f6
YanPanMichael <yanpancsu@icloud.com>
2018-02-01 00:05:59 +0800

    version 5

---
+++ src/com/duolufenfa/version5/RoShamBo5.java
@@ -0,0 +1,33 @@
+package com.duolufenfa.version5;
+
+import java.util.EnumMap;
+
+import com.duolufenfa.version1.Outcome;
+import com.duolufenfa.version2.Competitor;
+import com.duolufenfa.version2.RoShamBo;
+
+public enum RoShamBo5 implements Competitor<RoShamBo5>{
+    PAPER,SCISSORS,ROCK;
+    static EnumMap<RoShamBo5,EnumMap<RoShamBo5,Outcome>> table =
+        new EnumMap<RoShamBo5,EnumMap<RoShamBo5,Outcome>>(RoShamBo5.class);
+    static{
+        for(RoShamBo5 it:RoShamBo5.values())
+            table.put(it,new EnumMap<RoShamBo5,Outcome>(RoShamBo5.class));
+        initRow(PAPER,Outcome.DRAW,Outcome.LOSE,Outcome.WIN);
+        initRow(SCISSORS,Outcome.WIN,Outcome.DRAW,Outcome.LOSE);
+        initRow(ROCK,Outcome.LOSE,Outcome.WIN,Outcome.DRAW);
+    }
+    static void initRow(RoShamBo5 it,
+        Outcome vPAPER,Outcome vSCISSORS,Outcome vROCK){
+        EnumMap<RoShamBo5,Outcome> row=RoShamBo5.table.get(it);
+        row.put(RoShamBo5.PAPER,vPAPER);
+        row.put(RoShamBo5.SCISSORS,vSCISSORS);
+        row.put(RoShamBo5.ROCK,vROCK);
+    }
+    public Outcome compete(RoShamBo5 it){
+        return table.get(this).get(it);
+    }
+    public static void main(String[] args){
+        RoShamBo.play(RoShamBo5.class,20);
+    }
+}
